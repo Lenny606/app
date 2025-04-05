492fee54453b5767495d167020e56907a6a27645
Tomas Kravcik <kravcik@procorp.cz>
2025-04-05 13:50:34 +0200

    Add Comment model and migration
    Introduce a `Comment` model with MongoDB support and a migration for the `comments` table. The model establishes relationships with `User` and `Post` while specifying fillable attributes.

---
+++ app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Relations\BelongsTo;
+use MongoDB\Laravel\Relations\HasMany;
+
+class Comment extends Model
+{
+    protected $connection = 'mongodb';
+    protected $table = 'users'; //collection
+
+    protected $fillable = ['comment', 'user_id', 'post_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
