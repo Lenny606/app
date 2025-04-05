c7684958edf03b4193e842ad3a995f2d5aff8ee8
Tomas Kravcik <kravcik@procorp.cz>
2025-04-05 13:48:54 +0200

    Add Post model and migration for MongoDB integration
    Created a `Post` model and its corresponding migration. The model uses a MongoDB connection and defines relationships with `User`, `Comment`, and `Like`. The migration sets up a basic `posts` collection schema.

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+//use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Relations\BelongsTo;
+use MongoDB\Laravel\Relations\HasMany;
+
+class Post extends Model
+{
+    protected $connection = 'mongodb';
+    protected $table = 'users'; //collection
+
+    protected $fillable = ['caption', 'image_path', 'image_url', 'user_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+}
